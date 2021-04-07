230b3a26c851ab9031fa03f3a9e90dd915a8638c
Stewart ilondanga <stilondanga@gmail.com>
2021-04-07 17:06:51 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*var navigate = (function() {
+var navigate = (function() {
   $('.dd').toggle();
   $('.dd_btn').click(function() {
     var dataName = $(this).attr('data-name');
@@ -7,7 +7,7 @@
   });
 })();
 
-console.clear();
+/*console.clear();
 const container = document.querySelector('.container');
 
 const crossword =
